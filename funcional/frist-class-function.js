029a66e7a7e07987041c2fbe4dab1b60bd79cbe5
Pinto Júnior <junior_pinto@hotmail.com>
2022-03-22 15:26:27 -0300

    Frist-Class Function

---
+++ funcional/frist-class-function.js
@@ -0,0 +1,21 @@
+/*
+* A programming lanfuage is said to have
+* Frist-class functions when functions in
+* that language are treated like any other
+* variable.
+*/
+
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = (a, b) => a - b;
+
+const multiply = (x, y) => x * y;
+
+const divide = (a, b) => a / b;
+
+console.log(add(10, 20));
+console.log(subtract(10, 20));
+console.log(multiply(10, 20));
+console.log(divide(10, 20));
